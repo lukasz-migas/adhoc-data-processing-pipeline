180b80a98b987eb769f6c4da31540b9ea4e0e66e
Teresa Murta <teresa.murta@npl.co.uk>
2020-09-17 10:57:09 +0100

    documentation updates plus saving spectral details correction

--- f_saving_spectra_details.m
+++ f_saving_spectra_details.m
@@ -1,25 +1,49 @@
+% * This function performs:*
+% - data pre-processing
+% - total spectrum computation
+%
+% *Inputs:*
+% filesToProcess - Matlab structure created by the function “dir” which 
+% contains the list of files to process and their locations (paths)
+% preprocessing_file - SpectralAnalysis pre-processing file
+% mask - Name of the mask to be used to reduce the data to a particular 
+% group of pixels of interest. This name can  be either “no mask”, in which
+% case all pixels of the imzml file are used, or a name of a folder saved 
+% in a folder called “rois” which is part of the folders created by the 
+% pipeline
+% 
+% *Outputs:*
+% totalSpectrum_intensities - total spectrum counts (per imzml and mask)
+% totalSpectrum_mzvalues - total spectrum mass channels (per imzml and mask)
+% pixels_num - number of pixels of interest (per imzml and mask)
+
 function f_saving_spectra_details( filesToProcess, preprocessing_file, mask_list)
-% for each file to process
+
 for file_index = 1:length(filesToProcess)
-    % find the inputs file in the folder of origin
+
     csv_inputs = [ filesToProcess(file_index).folder '\inputs_file' ];
-    % from the inputs file, take the outputs path
+    
+    % Read outputs path from "inputs_file.xlsx"
+
     [ ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, ~, outputs_path ] = f_reading_inputs(csv_inputs);
-    % create the path at which you will output the ROIS
+    
+    % Define rois and spectral details paths
+
     rois_path               = [ char(outputs_path) '\rois\' ];
-    % create the path at which you will save the spectra details, if there
-    % is no folder with the said path, create one
     spectra_details_path    = [ char(outputs_path) '\spectra details\' ]; if ~exist(spectra_details_path,'dir'); mkdir(spectra_details_path); end
-    % for each type of mask requested
+
     for mask_type = mask_list
-        %  create a folder at the requiered location
+        
+        % Create and move to the folder where the spectral details of the 
+        % dataset will be saved. The representative spectrum will differ
+        % depending on the mask used.
+        
         mkdir([ spectra_details_path filesToProcess(file_index).name(1,1:end-6) '\' char(mask_type) '\'])
-        % relocate yourself in this folder
         cd([ spectra_details_path filesToProcess(file_index).name(1,1:end-6) '\' char(mask_type) '\'])
         
-        % Pre-processing of data, representative spectrum computation, and pick peaking
+        % Data pre-processing and total spectrum computation
         
-        disp('! Running pre-processing and generating representative spectra...')
+        disp('! Running pre-processing and generating total spectra...')
         
         addJARsToClassPath()
         preprocessing = PreprocessingWorkflow;
@@ -27,15 +51,11 @@ for file_index = 1:length(filesToProcess)
         
         parser = ImzMLParser([filesToProcess(file_index).folder filesep filesToProcess(file_index).name]);
         parser.parse; % parse the imzML file
-        
-        % Total Spectrum
-        
+                
         spectrumGeneration = TotalSpectrum();
-        spectrumGeneration.setPreprocessingWorkflow(preprocessing); % set preprocessing workflow
-
-        addlistener(spectrumGeneration, 'FastMethods', @(src, canUseFastMethods)disp(['! Using fast Methods? A: ' num2str(canUseFastMethods)]));
+        spectrumGeneration.setPreprocessingWorkflow(preprocessing); % setting preprocessing workflow
         
-        % ROI
+        % Data reduction based on the pixels of interest
         
         data = DataOnDisk(parser);
         
@@ -43,7 +63,7 @@ for file_index = 1:length(filesToProcess)
         
         if ~isequal(mask_type,"no mask")
             
-            load([ rois_path filesToProcess(file_index).name(1,1:end-6)  '\' char(mask_type) '\roi' ]) % loading region of interest mask
+            load([ rois_path filesToProcess(file_index).name(1,1:end-6)  '\' char(mask_type) '\roi' ]) % loading region of interest
             
             roiList = RegionOfInterestList;
             roiList.add(roi);
@@ -54,11 +74,13 @@ for file_index = 1:length(filesToProcess)
             
         end
                 
-        totalSpectrum = spectrumGeneration.process(data); % create total spectrum
+        totalSpectrum = spectrumGeneration.process(data); % computing total spectrum
         totalSpectrum = totalSpectrum.get(1);
         
         totalSpectrum_mzvalues      = totalSpectrum.spectralChannels;
         totalSpectrum_intensities   = totalSpectrum.intensities;
+        
+        % Saving outputs
         
         save('totalSpectrum_mzvalues','totalSpectrum_mzvalues','-v7.3')
         save('totalSpectrum_intensities','totalSpectrum_intensities','-v7.3')
